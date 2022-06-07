50051d74bf70aace700cfc9f94db6896d1500cac
Dagoberto Peralta <dagoberto.peralta@imexhs.com>
2022-06-07 11:31:24 -0500

    Add Scroll view and ViewModel aplication

--- app/src/main/java/com/example/viewmodel/MainActivity.kt
+++ app/src/main/java/com/example/viewmodel/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.viewmodel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.example.viewmodel.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var viewModel: MainActivityViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        saveInformation()
+
+    }
+
+    fun saveInformation() {
+        with(binding) {
+            txView.text = viewModel.getCurrentInformation()
+            btSend.setOnClickListener {
+                txView.text = viewModel.getTxtInformation("${etDocument.text}", "${etAge.text}")
+            }
+        }
     }
 }
--- app/src/main/java/com/example/viewmodel/MainActivityViewModel.kt
+++ app/src/main/java/com/example/viewmodel/MainActivityViewModel.kt
@@ -3,5 +3,15 @@ package com.example.viewmodel
 import androidx.lifecycle.ViewModel
 
 class MainActivityViewModel : ViewModel() {
-    get
+
+    private var txtInformation: String = ""
+
+    fun getCurrentInformation(): String{
+        return txtInformation
+    }
+
+    fun getTxtInformation(name: String, age:String): String{
+        txtInformation = "El texto es $name y la edad es $age"
+        return txtInformation
+    }
 }
